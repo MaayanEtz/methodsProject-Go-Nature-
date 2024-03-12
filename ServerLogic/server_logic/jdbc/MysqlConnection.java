6b8852f694a1491a28648d76eb67a4692c304551
MaayanEtz <maayan11etzioni@gmail.com>
2024-03-12 19:02:42 +0200

    create new branch from oren's

--- ServerLogic/server_logic/jdbc/MysqlConnection.java
+++ ServerLogic/server_logic/jdbc/MysqlConnection.java
@@ -21,7 +21,7 @@ public class MysqlConnection {
 		try {
 			//Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/test?serverTimezone=IST", "root", "Aa123456");
 			
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/go_nature_db?serverTimezone=IST","root","Ob55280772zazi!");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/test?serverTimezone=IST","root","123456");
 			System.out.println("SQL connection succeed");
 			return conn;
 		} catch (SQLException ex) {/* handle any errors */
